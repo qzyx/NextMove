87a036e817c414371a69ec511c8eea781d200df2
Martin Lazebnik <martin.lazebnik3@gmail.com>
2025-03-05 12:05:06 +0100

    Imported font

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
 import SpaceScene from "./_Components/SpaceTheme/SpaceTheme";
 
-export default function Home(){
-  return <main className="w-full h-screen">
-    <SpaceScene></SpaceScene>
-    Home</main>
+export default function Home() {
+  return (
+    <main className="w-full h-screen flex justify-center items-center">
+      <SpaceScene></SpaceScene>
+      <button className="px-5 py-3 text-3xl bg-black/70  rounded-md border-[1px]">Queue</button>
+    </main>
+  );
 }
